4fdc4398cc646d644fba29549e81d56037e3e541
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-02-01 09:21:06 -0600

    made cards print to page

--- javascripts/CarLot.js
+++ javascripts/CarLot.js
@@ -1,13 +1,30 @@
 var CarLot = (function () {
   var inventory = [];
+  var cardDisplay = document.getElementById("card-display");
 
   return {
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
-
-      inventoryLoader.addEventListener("load", function () {
-
+      inventoryLoader.open("GET", "inventory.json");
+	  inventoryLoader.send();
+      inventoryLoader.addEventListener("error", function(){
+      	alert("Error Loading!!! help...")
       });
+      inventoryLoader.addEventListener("load", function () {
+		var carData = JSON.parse(event.target.responseText);
+		var productObject = carData.cars;
+		
+		CarLot.showCarData(productObject);
+      })
+  	},
+    showCarData: function(x){
+    	for(var k in x) {
+    		var carCard = ``;
+			var carObject = x[k];
+			carCard += `<article class="col-sm-4"><div class="thumbnail"><h4>${carObject.make} - ${carObject.model}</h4><p>${carObject.description}</p><footer>${carObject.year}     for $${carObject.price}</footer></div></article>`;
+			cardDisplay.innerHTML += carCard;
+
+    	}
     }
   };
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,14 @@
 function populatePage (inventory) {
   // Loop over the inventory and populate the page
 
+
+
+
+
+
+
+
+
   // Now that the DOM is loaded, establish all the event listeners needed
   CarLot.activateEvents();
 }
